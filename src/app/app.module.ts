eedf0238b2b2be4fc42ac500c74ca330f7d3ee1d
Miguel Lima <miguellt@dcc.ufrj.br>
2022-11-25 12:07:39 -0300

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +10,7 @@ import { HomeComponent } from './home/home.component';
 import { AddClinicComponent } from './add-clinic/add-clinic.component';
 import { EditClinicComponent } from './edit-clinic/edit-clinic.component';
 import { ViewClinicComponent } from './view-clinic/view-clinic.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,7 +22,11 @@ import { ViewClinicComponent } from './view-clinic/view-clinic.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]

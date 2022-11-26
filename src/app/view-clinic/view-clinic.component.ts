4e053286b1334746f8571e7d1ed58aafa2fa9984
Miguel Lima <miguellt@dcc.ufrj.br>
2022-11-26 01:26:29 -0300

    view clinic

--- src/app/view-clinic/view-clinic.component.ts
+++ src/app/view-clinic/view-clinic.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpProviderService } from '../Service/http-provider.service';
+import { WebApiService } from '../Service/web-api.service';
 
 @Component({
   selector: 'app-view-clinic',
   templateUrl: './view-clinic.component.html',
   styleUrls: ['./view-clinic.component.css']
 })
-export class ViewClinicComponent {
+export class ViewClinicComponent implements OnInit {
 
+  id: any;
+  clinicDetail: any = [];
+
+  constructor (private httpProvider: HttpProviderService, private webApiService: WebApiService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.getClinicDetailById();
+  }
+
+  getClinicDetailById() {       
+    this.httpProvider.getClinicDetailById(this.id).subscribe((data : any) => {      
+      if (data != null && data.body != null) {
+        var resultData = data.body;
+        if (resultData) {
+          this.clinicDetail = resultData;
+        }
+      }
+    },
+    (error :any)=> { }); 
+  }
 }
